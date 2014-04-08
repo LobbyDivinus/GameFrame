95d6e796969a4dc37478c22c849f4ebe55fcc1af
Lobby Divinus <fe-software@gmx.net>
2014-04-08 12:12:26 +0200

    Add hiding titlebar in android <= 10.

--- src/info/flowersoft/gameframe/AppActivity.java
+++ src/info/flowersoft/gameframe/AppActivity.java
@@ -1,11 +1,21 @@
 package info.flowersoft.gameframe;
 
-import info.flowersoft.gameframe.AppRenderer;
-import info.flowersoft.gameframe.AppView;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 
+/**
+ * Base Activity class you can extend to get a real simple class. You just have to call the super constructor
+ * in your own constructor and override getRenderer() to return your preferred AppRenderer.
+ * 
+ * @author Lobby
+ */
 public abstract class AppActivity extends Activity {
 
 	private static AppRenderer renderer;
@@ -16,7 +26,7 @@ public abstract class AppActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		//getActionBar().hide();
+		hideActionBar();
 		
 		if (renderer == null) {
 			renderer = getRenderer(savedInstanceState);
@@ -44,6 +54,21 @@ public abstract class AppActivity extends Activity {
 		renderer.onResume();
 	}
 	
+	@SuppressLint("NewApi")
+	private void hideActionBar() {
+		if (Build.VERSION.SDK_INT >= 11) {
+			getActionBar().hide();
+		} else {
+			requestWindowFeature(Window.FEATURE_NO_TITLE);
+		}
+	}
+	
+	/**
+	 * Override this method to return your preferred AppRenderer. If your renderer overrides the constructor of
+	 * AppRenderer this may look like <b>return new MyRenderer(savedInstanceState, this);</b>.
+	 * @param savedInstanceState instance state the app got on start
+	 * @return own app renderer
+	 */
 	public abstract AppRenderer getRenderer(Bundle savedInstanceState);
 
 }
