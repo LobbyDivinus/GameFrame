0f98bbf7a561f5713f35377b27835fac5144e5f2
Lobby Divinus <fe-software@gmx.net>
2014-12-23 23:53:01 +0100

    Add screen shape that will contain every touch point.

---
+++ src/info/flowersoft/gameframe/description/ScreenEverywhere.java
@@ -0,0 +1,10 @@
+package info.flowersoft.gameframe.description;
+
+public class ScreenEverywhere implements ScreenShape {
+
+	@Override
+	public boolean contains(int x, int y) {
+		return true;
+	}
+
+}
